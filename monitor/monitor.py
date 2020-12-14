1f0d50123f124d75552af7cb3030a6b1209adc86
SnappyDragon64 <pg61151@gmail.com>
2020-12-14 20:53:37 +0530

    Update monitor.py

--- monitor/monitor.py
+++ monitor/monitor.py
@@ -185,7 +185,7 @@ class Monitor(Cog):
             if channel_dict[message.guild.id] is not None:
                 channel_id = channel_dict[message.guild.id]
                 em = discord.Embed(title="Message sent", description=message, color=discord.Color.blue()
-                avatar = message.author.avatar ? message.author.avatar_url : message.author.default_avatar_url
+                avatar = (message.author.avatar is not None) ? message.author.avatar_url : message.author.default_avatar_url
                 em.set_author(name='**{}**'.format(author), icon_url=avatar))
                 await self.bot.send_message(channel, embed=em)
                 
